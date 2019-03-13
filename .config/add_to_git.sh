bd78af98a2531b100d3ffb687ee02954d7348520
Artem Zinchenko <artemhb@gmail.com>
2019-03-12 22:44:48 -0700

    upd

--- .config/add_to_git.sh
+++ .config/add_to_git.sh
@@ -26,6 +26,7 @@ git add -f ~/.config/kitty/kitty.conf
 git add -f ~/.config/omf
 git add -f ~/.config/vim/theme
 git add -f ~/.gitignore
+git add -f ~/.jupyter/custom/custom.css
 git add -f ~/.numpy-site.cfg
 git add -f ~/.tmux.conf
 git add -f ~/.vim/after
